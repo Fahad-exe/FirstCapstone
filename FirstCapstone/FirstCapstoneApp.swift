b68fa831943f5c851c43524947f00f003448ea7b
Fahad Matlagitu <matlagitu@gmail.com>
2023-11-23 14:26:38 +0300

    initialise Capstone project

---
+++ FirstCapstone/FirstCapstoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstCapstoneApp.swift
+//  FirstCapstone
+//
+//  Created by Fahad Matlagitu on 23/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct FirstCapstoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
